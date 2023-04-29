996fb2762e38711c30be0e9e1e0fe180e2189291
Alan Vale <valealan84@gmail.com>
2023-04-29 13:02:56 -0300

    Add files via upload

--- control.lua
+++ control.lua
@@ -1,38 +1,66 @@
+-- Falta deixar invisivel e imune a belt
 playersMoving = {}
 
 function movePlayers()
   for _, playerData in pairs(playersMoving) do
     local player = playerData.player
     local direction = playerData.direction
-    player.walking_state = {walking = true, direction = direction}
+    player.character_running_speed_modifier = 0.3
+    if playerData.flag == 0 then
+      player.walking_state = {walking = true, direction = direction}
+      playerData.flag = playerData.spd
+    else 
+      player.walking_state = {walking = false, direction = direction}
+      playerData.flag = playerData.flag - 1
+    end
+
   end
+end
+
+function setUnderground(player, belt, spd)
+  setInvisible()
+  playersMoving[player.index] = { player = player, direction = belt.direction, output = belt.neighbours, spd = spd, flag = spd, flashOn = player.is_flashlight_enabled() }
+  player.disable_flashlight()
+end
+
+function resetPlayer(player, belt)
+  setVisible()
+  local playerData = playersMoving[player.index]
+  if playerData.flashOn then player.enable_flashlight() end
+  playersMoving[player.index] = nil
 end
 
 function getBeltUnder(player)
   local px, py = player.position.x, player.position.y
-  local ps = 0.15 -- Half of player size (0.5)
+  local ps = 0.1
 
   --                Start X and Y  |  End X and Y
   local beltArea = {{px-ps, py-ps}, {px+ps, py+ps}}
   local belt = player.surface.find_entities_filtered({area = beltArea, name = 'underground-belt', limit = 1})[1]
   if belt then
-    return belt
+    return { belt = belt, spd = 2 }
+  end
+  local belt = player.surface.find_entities_filtered({area = beltArea, name = 'fast-underground-belt', limit = 1})[1]
+  if belt then
+    return { belt = belt, spd = 1 }
+  end
+  local belt = player.surface.find_entities_filtered({area = beltArea, name = 'express-underground-belt', limit = 1})[1]
+  if belt then
+    return { belt = belt, spd = 0 }
   end
 end
 
 function checkPlayersOverBelt()
   for _, player in pairs(game.players) do
-    local belt = getBeltUnder(player)
+    local beltData = getBeltUnder(player)
 
-    if belt then
+    if beltData then
+      local belt = beltData.belt
+      local spd = beltData.spd
       if belt.belt_to_ground_type == 'input' and not playersMoving[player.index] and belt.neighbours then
-        playersMoving[player.index] = { player = player, direction = belt.direction, output = belt.neighbours }
-        local existing_sprite = rendering.get_sprite(1)
-        local custom_sprite = table.deepcopy(existing_sprite)
-        custom_sprite.tint = {r = 1, g = 0, b = 0, a = 0}
-        player.set_render_mode{path_animation = nil, layers = {custom_sprite}}
-      elseif belt.belt_to_ground_type == 'output' and playersMoving[player.index] and playersMoving[player.index].output == belt then
-        playersMoving[player.index] = nil
+        setUnderground(player, belt, spd)
+      elseif playersMoving[player.index] and playersMoving[player.index].output == belt then
+        resetPlayer(player, belt)
       end
     end
   end
@@ -44,3 +72,9 @@ function onTick()
 end
 
 script.on_event(defines.events.on_tick, onTick)
+
+function print(msg, use_serpent)
+  use_serpent = use_serpent or true
+  if use_serpent then game.print(serpent.block(msg))
+  else game.print(tostring(msg)) end
+end
---
+++ load.bat
@@ -0,0 +1,4 @@
+@rem THIS PATH / FACTORIO MOD PATH
+xcopy C:\Main\Programming\Lua\Factorio-Mods\amazingBelt_0.0.1 C:\Users\Alanvv\AppData\Roaming\Factorio\mods\amazingBelt_0.0.1 /y
+@rem FACTORIO IN PATH / MY SAVE FOR TEST
+factorio --load-game C:\Users\Alanvv\AppData\Roaming\Factorio\saves\test.zip 
