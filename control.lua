d5fef4f122ec7a42bfa406327e2a2ddf2ca0594d
Alan Vale <valealan84@gmail.com>
2023-06-20 22:33:46 -0300

    Update control.lua

--- control.lua
+++ control.lua
@@ -1,75 +1,51 @@
-playersUndergroung = {}
+undergroundPlayersData = {}
 
-function setUnderground(player, belt, spd)
-  playersUndergroung[player.index] = { player = player, direction = belt.direction, output = belt.neighbours, spd = spd, flag = spd, flashOn = player.is_flashlight_enabled() }
-  player.disable_flashlight()
+function setUnderground(player, belt)
+  local char = player.character
+  local car = char.surface.create_entity({
+      name = 'invisible-car',
+      position = char.position,
+      force = char.force,
+      direction = belt.direction
+  })
+  car.speed = belt.prototype.belt_speed
+  player.driving = true
+
+  undergroundPlayersData[player.index] = { entrance = belt, exit = belt.neighbours, car = car }
 end
 
 function setOverground(player, belt)
-  local playerData = playersUndergroung[player.index]
-  if playerData.flashOn then player.enable_flashlight() end
-  playersUndergroung[player.index] = nil
+  player.vehicle.destroy()
+  player.driving = false
+  player.teleport(belt.position)
+
+  undergroundPlayersData[player.index] = nil
 end
 
-function getBeltUnder(player)
-  local px, py = player.position.x, player.position.y
-  local ps = 0.1
+function goBackToEntrance(player)
+  local data = undergroundPlayersData[player.index]
+  player.teleport(data.entrance.position)
+  data.car.destroy()
 
-  --                Start X and Y  |  End X and Y
-  local beltArea = {{px-ps, py-ps}, {px+ps, py+ps}}
-  local belt = player.surface.find_entities_filtered({area = beltArea, name = 'underground-belt', limit = 1})[1]
-  if belt then
-    return { belt = belt, spd = 2 }
-  end
-  local belt = player.surface.find_entities_filtered({area = beltArea, name = 'fast-underground-belt', limit = 1})[1]
-  if belt then
-    return { belt = belt, spd = 1 }
-  end
-  local belt = player.surface.find_entities_filtered({area = beltArea, name = 'express-underground-belt', limit = 1})[1]
-  if belt then
-    return { belt = belt, spd = 0 }
-  end
+  undergroundPlayersData[player.index] = nil
 end
 
-function checkPlayersOverInputBelt()
-  for _, player in pairs(game.players) do
-    local beltData = getBeltUnder(player)
+script.on_event(defines.events.on_player_changed_position, function(e)
+  local player = game.players[e.player_index]
 
-    if not beltData then return end
+  local belt = player.surface.find_entities_filtered({ position = player.position, radius = 1, type = 'underground-belt' })[1]
+  if not belt then return end
 
-    local belt = beltData.belt
-    local speed = beltData.speed
-
-    -- Is an input + Player is over ground + Belt has an exit
-    if belt.belt_to_ground_type == 'input' and not playersUndergroung[player.index] and belt.neighbours then
-      setUnderground(player, belt, speed)
-    -- Player is under ground + Belt is the exit
-    elseif playersUndergroung[player.index] and playersUndergroung[player.index].output == belt then
-      setOverground(player, belt)
-    end
+  if not player.vehicle and belt.belt_to_ground_type == 'input' then setUnderground(player, belt)
+  elseif player.vehicle and player.vehicle.name == 'invisible-car' and belt == undergroundPlayersData[player.index].exit then setOverground(player, belt) 
   end
-end
 
-function moveUndergroundPlayers()
-  for _, playerData in pairs(playersUndergroung) do
-    local player = playerData.player
-    local direction = playerData.direction
-    player.character_running_speed_modifier = 0.3
-    if playerData.flag == 0 then
-      player.walking_state = {walking = true, direction = direction}
-      playerData.flag = playerData.spd
-    else 
-      player.walking_state = {walking = false, direction = direction}
-      playerData.flag = playerData.flag - 1
-    end
-  end
-end
+  if not player.vehicle and undergroundPlayersData[player.index] then goBackToEntrance(player) end
+end)
+script.on_event(defines.events.on_player_driving_changed_state, function (e)
+  local player = game.players[e.player_index]
 
-function onTick() 
-  checkPlayersOverInputBelt()
-  moveUndergroundPlayers()
-end
+  if not undergroundPlayersData[player.index] then return end
 
--- 
-
-script.on_event(defines.events.on_tick, onTick)
+  goBackToEntrance(player)
+end)
